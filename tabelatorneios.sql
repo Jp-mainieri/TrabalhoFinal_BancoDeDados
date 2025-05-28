4a64128483f25ababa40a4a8e18b6e5137276357
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-05-28 10:44:14 -0300

    tabela torneios feita

--- tabelatorneios.sql
+++ tabelatorneios.sql
@@ -1,1 +1,8 @@
-CREATE Table torneios(Id_torneios INT PRIMARY KEY,)
+CREATE Table torneios(
+    Id_torneios INT PRIMARY KEY,
+    nome_torneios Varchar(100),
+    jogo VARCHAR(100),
+    premiacao_torneios Float,
+    data_inicio DATE,
+    data_final DATE,
+)
