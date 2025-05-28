4b7e98f2c47d4f08219195326d1031702fe06ec4
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-05-28 11:07:12 -0300

    jogos + torneio

--- tabelatorneios.sql
+++ tabelatorneios.sql
@@ -1,4 +1,5 @@
 -- Active: 1748438097855@@BD-ACD@3306@BD180225117
+use BD180225117;
 CREATE Table torneios(
     Id_torneios INT PRIMARY KEY AUTO_INCREMENT,
     nome_torneio Varchar(100),
@@ -7,14 +8,14 @@ CREATE Table torneios(
     data_inicio DATE,
     data_final DATE
 );
-ALTER TABLE
+ALTER TABLE torneios ADD FOREIGN KEY (jogo) REFERENCES jogos(Id_jogo);
 INSERT INTO torneios(nome_torneio,jogo,premiacao_torneios,data_inicio,data_final) VALUES ()
 VALUES
-('Torneio 1', 'League of Legends', 1000.00, '2022-01-01', '2022-01-10'),
-('Torneio 2', 'Valorant', 800.50, '2022-02-05', '2022-02-15'),
-('Torneio 3', 'Counter-Strike: Global Offensive', 1500.75, '2022-03-10', '2022-03-20'),
-('Torneio 4', 'Dota 2', 1200.25, '2022-04-15', '2022-04-25'),
-('Torneio 5', 'Overwatch', 950.80, '2022-05-20', '2022-05-30'),
+('Torneio 1', 1000.00, '2022-01-01', '2022-01-10'),
+('Torneio 2', 800.50, '2022-02-05', '2022-02-15'),
+('Torneio 3', 1500.75, '2022-03-10', '2022-03-20'),
+('Torneio 4', 1200.25, '2022-04-15', '2022-04-25'),
+('Torneio 5', 950.80, '2022-05-20', '2022-05-30'),
 ('Torneio 6', 'Rainbow Six Siege', 1100.30, '2022-06-25', '2022-07-05'),
 ('Torneio 7', 'Fortnite', 700.60, '2022-07-30', '2022-08-10'),
 ('Torneio 8', 'Apex Legends', 850.40, '2022-08-15', '2022-08-25'),
